66ee806d84c38683441641d535976d87efcf3b54
Yogeshwaran <yogeshwaran08083@gmail.com>
2024-12-16 19:30:01 +0530

    added:backend

--- src/constants/backend.ts
+++ src/constants/backend.ts
@@ -1,1 +1,2 @@
-export const backend_url = "http://localhost:5000/api/v1";
+// export const backend_url = "http://localhost:5000/api/v1";
+export const backend_url = "https://api.smmenterprise.com/api/v1";
